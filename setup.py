627e65ba554905ba711ca08ce0340d0f772d6aa9
bzdvdn <bzdv.dn@gmail.com>
2019-12-04 09:58:39 +0300

    add MLGException to __init__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='medialogia-api-wrapper',
-    version='0.0.1',
+    version='0.0.1.1',
     packages=find_packages(),
     install_requires=[
         'zeep>=3.4.0'
